d38b44517a39974550cb2f2cf7efd8e7c5db093f
pengxuanlan <34526988+pengxuanlan@users.noreply.github.com>
2024-08-23 09:59:12 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["oa.yarou.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "LAjE3K0zWxRDx8VleAooTzJwhDdASlBHW9dEnqUE8a8=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[""];
+pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
